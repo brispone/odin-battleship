de91181dc3b4bef069a4c15bf8bed29089386ec3
Bradley Rispone <bradrispone@gmail.com>
2023-11-11 14:23:22 -0600

    Add more info to gameboard positions to faciliate logging hits and misses

--- src/gameboardFactory.js
+++ src/gameboardFactory.js
@@ -5,7 +5,7 @@ function createGameboard() {
     // Create a 2-dimensional array for the gameboard - each cell initalizaed as null to signify that it is empty
     // Outer arrays are the vertical, inner is horizontal
     // ie: Position A1 would be board[0][0]
-    const board = [
+    /*const board = [
         [null, null, null, null, null, null, null, null, null, null],
         [null, null, null, null, null, null, null, null, null, null],
         [null, null, null, null, null, null, null, null, null, null],
@@ -16,7 +16,11 @@ function createGameboard() {
         [null, null, null, null, null, null, null, null, null, null],
         [null, null, null, null, null, null, null, null, null, null],
         [null, null, null, null, null, null, null, null, null, null]
-    ]
+    ]*/
+
+    // Initialize the gameboard, 2-dimensional array that can be accessed like board[column][row] - status should be empty, miss, or hit
+    const initializeEmptyCell = () => ({ ship: null, status: 'empty' });
+    const board = Array.from({ length: 10 }, () => Array.from({ length: 10 }, initializeEmptyCell));
 
     return {
         board: board,
@@ -33,7 +37,7 @@ function createGameboard() {
 
             // Place the ship at the relevant coordinates
             for(let i = 0; i < newShip.length; i++) {
-                this.board[ycoord][xcoord] = newShip;
+                this.board[ycoord][xcoord].ship = newShip;
                 if(orientation == "vertical") {
                     ycoord++;
                 } else {
@@ -43,10 +47,13 @@ function createGameboard() {
 
         },
         recieveAttack: function(ycoord, xcoord) {
-            const positionContent = this.board[ycoord][xcoord];
-            if (positionContent) {
-                positionContent.hit();
-            } else console.log('Missed!');
+            const ship = this.board[ycoord][xcoord].ship;
+            if (ship) {
+                ship.hit();
+            } else {
+                console.log("Missed!");
+
+            }
         }
     }
 }
@@ -58,7 +65,7 @@ function checkCells(gameboard, y, x, length, orientation) {
 
     // Traverse the relevant coordinates and make sure they are empty
     for(let i = 0; i < length; i++) {
-        if (gameboard[ycoord][xcoord]) {
+        if (gameboard[ycoord][xcoord].ship) {
             return false;
         }
         if(orientation == "vertical") {
